f532446e81f600b2c430811d076b107fed288690
Ryan Underwood <runderworld@gmail.com>
2021-11-03 22:33:18 -0700

    Getting closer to fixing BaseRate once and for all.

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,10 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+function perc(num) {
+    return num/100;
+}
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
@@ -130,16 +134,15 @@ function onDeviceReady() {
         $('form#calxForm').calx({
             data: {
                 /* beautify ignore:start */
-                //BEGIN: Pricing as of 6/11/21
-                J5: '',   K5: .5,    L5: .55,    M5: .6,    N5: .65,    O5: .7,    P5: .75,    Q5: .8,
-                J6: 760,  K6: 3.999,  L6: 4.125,  M6: 4.250,  N6: 4.250,  O6: 4.375,  P6: 4.500,  Q6: 4.625,
-                J7: 740,  K7: 4.125,  L7: 4.250,  M7: 4.375,  N7: 4.375,  O7: 4.500,  P7: 4.625,  Q7: 4.750,
-                J8: 720,  K8: 4.250,  L8: 4.375,  M8: 4.500,  N8: 4.500,  O8: 4.625,  P8: 4.750,  Q8: 4.999,
-                J9: 700,  K9: 4.375,  L9: 4.500,  M9: 4.625,  N9: 4.750,  O9: 4.875,  P9: 4.999,  Q9: 5.250,
-                J10: 680, K10: 4.500, L10: 4.625, M10: 4.750, N10: 4.875, O10: 5.125, P10: 5.250, Q10: 5.625,
-                J11: 660, K11: 4.875, L11: 4.999, M11: 5.125, N11: 5.375, O11: 5.625, P11: 5.625, Q11: 6.250,
-                J12: 640, K12: 5.250, L12: 5.375, M12: 5.500, N12: 5.750, O12: 5.999, P12: 6.250, Q12: 6.875,
-                //END
+                // Pricing as of 6/11/21
+                J5:  {value: ''},  K5:  {value: perc(50)},    L5:  {value: perc(55)},    M5:  {value: perc(60)},    N5:  {value: perc(65)},    O5:  {value: perc(70)},    P5:  {value: perc(75)},    Q5:  {value: perc(80)},
+                J6:  {value: 760}, K6:  {value: perc(3.999)}, L6:  {value: perc(4.125)}, M6:  {value: perc(4.250)}, N6:  {value: perc(4.250)}, O6:  {value: perc(4.375)}, P6:  {value: perc(4.500)}, Q6:  {value: perc(4.625)},
+                J7:  {value: 740}, K7:  {value: perc(4.125)}, L7:  {value: perc(4.250)}, M7:  {value: perc(4.375)}, N7:  {value: perc(4.375)}, O7:  {value: perc(4.500)}, P7:  {value: perc(4.625)}, Q7:  {value: perc(4.750)},
+                J8:  {value: 720}, K8:  {value: perc(4.250)}, L8:  {value: perc(4.375)}, M8:  {value: perc(4.500)}, N8:  {value: perc(4.500)}, O8:  {value: perc(4.625)}, P8:  {value: perc(4.750)}, Q8:  {value: perc(4.999)},
+                J9:  {value: 700}, K9:  {value: perc(4.375)}, L9:  {value: perc(4.500)}, M9:  {value: perc(4.625)}, N9:  {value: perc(4.750)}, O9:  {value: perc(4.875)}, P9:  {value: perc(4.999)}, Q9:  {value: perc(5.250)},
+                J10: {value: 680}, K10: {value: perc(4.500)}, L10: {value: perc(4.625)}, M10: {value: perc(4.750)}, N10: {value: perc(4.875)}, O10: {value: perc(5.125)}, P10: {value: perc(5.250)}, Q10: {value: perc(5.625)},
+                J11: {value: 660}, K11: {value: perc(4.875)}, L11: {value: perc(4.999)}, M11: {value: perc(5.125)}, N11: {value: perc(5.375)}, O11: {value: perc(5.625)}, P11: {value: perc(5.625)}, Q11: {value: perc(6.250)},
+                J12: {value: 640}, K12: {value: perc(5.250)}, L12: {value: perc(5.375)}, M12: {value: perc(5.500)}, N12: {value: perc(5.750)}, O12: {value: perc(5.999)}, P12: {value: perc(6.250)}, Q12: {value: perc(6.875)},
                 /* beautify ignore:end */
                 K14: {//ABS_Score
                     formula: 'IF(C13<660,"NA",IF(AND(C13<680,C13>=660),660,IF(AND(C13<700,C13>=680),680,IF(AND(C13<700,C13>=680),680,IF(AND(C13<720,C13>=700),700,IF(AND(C13<740,C13>=720),720,IF(AND(C13<760,C13>=740),740,IF(C13>=760,760))))))))',
@@ -150,8 +153,9 @@ function onDeviceReady() {
                 },
                 K17: {//Base_Rate  (A==>J, B==>K, ...)
                     // formula: 'INDEX(K6:Q12, MATCH(K14,J6:J12,-1), MATCH(K15,K5:Q5,1))',
-                    //SUMPRODUCT(vlookup_column_range = vlookup_value) * (hlookup_row_range = hlookup_value) * data_array)
-                    formula: 'SUMPRODUCT( (J6:J12=K14) * (K5:Q5=K15) * K6:Q12)',
+                    //        SUMPRODUCT(vlookup_col_range = vlookup_value) * (hlookup_row_range = hlookup_value) * data_array)
+                    // formula: 'SUMPRODUCT( (J6:J12=K14) * (K5:Q5=K15) * K6:Q12)',
+                    formula: 'SUMPRODUCT((J6:J12=K14) * (K5:Q5=K15) * K6:Q12)',
                     // format: '0.000%',
                 },
                 K19: {//TWO_to_FOUR_Units
